aae9738432eeb063ba2e8c35ddd325a8dec8af82
sjakhar <sunil.jakhar@soti.net>
2022-09-10 11:14:02 +0530

    Initial Commit

---
+++ MovieDatabase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDatabase
+//
+//  Created by Sunil Jakhar on 10/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
